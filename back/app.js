bef9a345cfe84a1ad56a3259d609e03d5b774b8a
17Amir17 <amirangel101@hotmail.com>
2021-12-01 15:54:18 +0200

    Added login and message router

--- back/app.js
+++ back/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const loginRouter = require('./routers/loginRouter');
 const errorHandler = require('./middleware/errorHandler/errorHandler');
 const app = express();
 
 //Middleware
 app.use(cors());
-
+//Routers
+app.use(loginRouter);
 app.use(errorHandler);
 
 module.exports = app;
