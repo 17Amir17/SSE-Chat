6c0366e7f47011a5b0adabfde8bd3d85c30f6f3e
17Amir17 <amirangel101@hotmail.com>
2021-12-01 17:15:09 +0200

    Added router

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SessionContextProvider from '../Context/SessionContext';
 import Login from './Login';
 
 function App() {
   return (
     <SessionContextProvider>
-      <div>
-        <Login />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Login />} />
+          <Route path={'/chat'} element={<div></div>} />
+        </Routes>
+      </BrowserRouter>
     </SessionContextProvider>
   );
 }
