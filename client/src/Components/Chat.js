30e9b54e22500de00282e2390c3c0c3a3fab61e2
17Amir17 <amirangel101@hotmail.com>
2021-12-01 17:44:57 +0200

    Styling additions

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
+import { SessionContext } from '../Context/SessionContext';
 import '../styles/chat.css';
 
 function Chat(props) {
+  const context = useContext(SessionContext);
   return (
     <div className={'chat'}>
-      <div className={'message-area'}>
-        <textarea className={'history'} readOnly={true}></textarea>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <textarea className={'messagebox'}></textarea>
-          <button className={'send'}>Send!</button>
+      <span style={{ textAlign: 'left', marginBottom: '0.5em' }}>
+        Logged in as {context.username}
+      </span>
+      <div className={'content'}>
+        <div className={'message-area'}>
+          <textarea className={'history'} readOnly={true}></textarea>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <textarea className={'messagebox'}></textarea>
+            <button className={'send'}>Send!</button>
+          </div>
         </div>
+        <ul className={'online'}></ul>
       </div>
-      <ul className={'online'}></ul>
     </div>
   );
 }
